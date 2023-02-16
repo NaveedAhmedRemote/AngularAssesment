2f76cc320bf2b0f4340d174531b502ef27ce40ee
naveedahmedremote <naveedahmedbeetech@gmail.com>
2023-02-16 20:03:21 +0500

    Firs Commit

---
+++ src/app/develop/develop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-develop',
+  templateUrl: './develop.component.html',
+  styleUrls: ['./develop.component.scss']
+})
+export class DevelopComponent {
+
+}
